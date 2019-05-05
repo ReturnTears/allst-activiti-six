51b4b4af5f7aa6a58cd9afe52bce34c3c7408f51
returntears <returntears@outlook.com>
2019-05-06 00:14:39 +0800

    activiti 6 的学习

---
+++ src/main/java/allst/activiti/six/AllstActivitiSixApplication.java
@@ -0,0 +1,13 @@
+package allst.activiti.six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllstActivitiSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllstActivitiSixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allst/activiti/six/AllstActivitiSixApplicationTests.java
@@ -0,0 +1,16 @@
+package allst.activiti.six;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllstActivitiSixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
